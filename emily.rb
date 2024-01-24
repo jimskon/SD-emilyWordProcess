3e096b42790d59f9605382dcde24d9db81fa321a
James Skön <skonjp@kenyon.edu>
2024-01-24 16:44:46 -0500

    Add starting code

--- emily.rb
+++ emily.rb
@@ -5,18 +5,49 @@
 # Read file and break into lines
 def getFile(name)
     fileObj = File.open(name)
-    fileObj.read()
+    text=fileObj.read()
     text.split("\n")
 end
 
+# Display all the line in an array of text strings
 def displayLines(lines)
   lines.each do |l|
     print l,"\n"
   end
 end
 
+# Return a hash mapping words the the count of their occurances
+# e.g. {"tree"=>4,"boat"=>1}
+def createWordCountTable(lines)
+  # Remove punctuation from w, and make lower case
+  # See https://www.ralfebert.com/snippets/ruby-rails/regex_cheat_sheet/
+  # w=w.gsub(/\W/, ' ').downcase  
+end
+
+# Given a word count table, ask for words, and lookup the
+# counts, until user enteres a empty line.
+def lookupWordCounts(countTable)
+
+end
+
+# Return a hash mapping words in lines to a list of line numbers where
+# the word appears.
+# e.g. {"tree"=>[5, 12, 56, 132],"boat"=>[325]}
+def createWordLineTable(lines)
+
+end
+
+# Given a word line table (from above), ask for words, and lookup and
+# display the lines with this word until a empty line is entered
+# Do not display the same line twice
+def printMatchingLines(wordLineTable)
+
+end
+
 lines = getFile("emilydickenson.txt")
 displayLines(lines)
+countTable=createWordCountTable(lines)
+lookupWordCounts(countTable)
 
-
-
+WordLineTable=createWordLineTable(lines)
+printMatchingLines(WordLineTable)
